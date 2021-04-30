9e1f3cfe4ea3a4b6f53af6e4de8a0cdf1d0f81fd
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-30 18:43:13 -0300

    Update PBIFreeLicenses.ps1

--- PBIFreeLicenses.ps1
+++ PBIFreeLicenses.ps1
@@ -1,36 +1,40 @@
-﻿# Script criado por Guilherme Matheus em 15/01/2021
-# Referencia da API retirada do link oficial https://docs.microsoft.com/en-us/powershell/module/azuread/?view=azureadps-2.0
-# Para funcionar, é necessario instalar alguns módulos, link: https://docs.microsoft.com/pt-br/powershell/azure/azurerm/install-azurerm-ps?view=azurermps-6.13.0
+# --------------------------------------------------------------------------------------#
+# Title: Delete files older than seven days
+# Author: Guilherme Matheus
+# Date: Script created on 30.04.2021
+# Script and data info: This script clean up a folder with files older than 1 week
+# --------------------------------------------------------------------------------------#
 
-# Para executar o script sem concordar com a politica de execução
+# Reference taken from the official link https://docs.microsoft.com/en-us/powershell/module/azuread/?view=azureadps-2.0
+# To work, it is necessary to install some modules, link: https://docs.microsoft.com/pt-br/powershell/azure/azurerm/install-azurerm-ps?view=azurermps-6.13.0
+
+# To execute the script without agreeing with the execution policy
 Set-ExecutionPolicy Bypass -Scope Process
 
-# Importa os modulos do Azure
+# Imports Azure modules
 Import-Module AzureRM
 Import-Module AzureAD
 
-# Dados da conta e do Azure para poder acessar
-$PBIAdminUPN = “userbi@vivest.com.br”
-$PBIAdminPW = “Law70638”
-$MyOrgTenantID = “8def9a7d-e13f-475b-bd57-f27619723591”
-$MyOrgBIAppID = “84e116a4-bfc9-4940-bbdb-c0d3fc94aa9a”
-$MyOrgBIThumbprint = “E24DF521C12DEDF98752A90A0FA47DD8D3589C9D”
+# Email and Azure account data to be able to connect
+$PBIAdminUPN = “youremail@email.com.br”
+$PBIAdminPW = “yourpassword”
+$MyOrgTenantID = “your Azure tenant”
+$MyOrgBIAppID = “your ID from your Azure app”
+$MyOrgBIThumbprint = “thumbprint id”
 
-
-#2. Executa a credencial de acordo com o login, senha e tenant ID acima
+# Run the credential according to the login, password and tenant ID above
 $SecPasswd = ConvertTo-SecureString $PBIAdminPW -AsPlainText -Force
 
 $myCred = New-Object System.Management.Automation.PSCredential($PBIAdminUPN,$SecPasswd)
 
-Connect-AzureAD -TenantId “8def9a7d-e13f-475b-bd57-f27619723591” -Credential $myCred
+Connect-AzureAD -TenantId “your Azure tenant” -Credential $myCred
 
-#3. Define o diretório e o nome do arquivo que será exportado o arquivo CSV
+# Defines the directory and name of the file to be exported to the CSV file
 $RetrieveDate = Get-Date 
-$BasePath = "D:\BI_Processos\PowerShell\Azure\"
-$UserPBIProLicensesCSV = $BasePath + "I_BI_PBILICENCASFREE.csv"
+$BasePath = "D:\Azure\"
+$UserPBIProLicensesCSV = $BasePath + "PBIFree.csv"
 
-#4. Obtem os usuarios das licenças
-
+# Get license users
 $ADUsers = Get-AzureADUser -All $true | Select-Object ObjectId, ObjectType, CompanyName, Department, DisplayName, JobTitle, Mail, Mobile, `
             SipProxyAddress, TelephoneNumber, UserPrincipalName, UserType, @{Name="Date Retrieved";Expression={$RetrieveDate}}
 
@@ -38,11 +42,11 @@ $ADUsers = Get-AzureADUser -All $true | Select-Object ObjectId, ObjectType, Comp
 See MS Licensing Service Plan reference: 
 https://docs.microsoft.com/en-us/azure/active-directory/users-groups-roles/licensing-service-plan-reference
 #>
-# Service Plan ID referente as licenças FREE do Power BI
+# Service Plan ID for Power BI FREE licenses
 $PBIFreeServicePlanID = "2049e525-b859-401b-b2a0-e0a31c4b1fe4"
 
-# Recupere e exporte usuários com licenças profissionais com base no Service Plan ID do Power BI Free ($PBIFreeServicePlanID)
-# Cada linha representa um plano de serviço para um usuário específico. Este detalhe da licença é filtrado apenas para a ID do plano de serviço do Power BI FREE.
+# Retrieve and export users with professional licenses based on the Power BI Free Service Plan ID ($PBIFreeServicePlanID)
+# Each line represents a service plan for a specific user. This license detail is filtered only for the Power BI FREE service plan ID.
 $UserLicenseDetail = ForEach ($ADUser in $ADUsers)
     {
         $UserObjectID = $ADUser.ObjectId
