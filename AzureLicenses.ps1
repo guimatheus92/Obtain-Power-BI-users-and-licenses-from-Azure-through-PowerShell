cec604a3fdc3ef04a5ac2502b12c3ff7ce0f5228
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-30 18:42:47 -0300

    Update AzureLicenses.ps1

--- AzureLicenses.ps1
+++ AzureLicenses.ps1
@@ -1,33 +1,38 @@
-﻿# Script criado por Guilherme Matheus em 15/01/2021
-# Referencia da API retirada do link oficial https://docs.microsoft.com/en-us/powershell/module/azuread/?view=azureadps-2.0
-# Para funcionar, é necessario instalar alguns módulos, link: https://docs.microsoft.com/pt-br/powershell/azure/azurerm/install-azurerm-ps?view=azurermps-6.13.0
+# --------------------------------------------------------------------------------------#
+# Title: Delete files older than seven days
+# Author: Guilherme Matheus
+# Date: Script created on 30.04.2021
+# Script and data info: This script clean up a folder with files older than 1 week
+# --------------------------------------------------------------------------------------#
 
-# Para executar o script sem concordar com a politica de execução
+# Reference taken from the official link https://docs.microsoft.com/en-us/powershell/module/azuread/?view=azureadps-2.0
+# To work, it is necessary to install some modules, link: https://docs.microsoft.com/pt-br/powershell/azure/azurerm/install-azurerm-ps?view=azurermps-6.13.0
+
+# To execute the script without agreeing with the execution policy
 Set-ExecutionPolicy Bypass -Scope Process
 
-# Importa os modulos do Azure
+# Imports Azure modules
 Import-Module AzureRM
 Import-Module AzureAD
 
 # Dados da conta e do Azure para poder acessar
-$PBIAdminUPN = “userbi@vivest.com.br”
-$PBIAdminPW = “Law70638”
-$MyOrgTenantID = “8def9a7d-e13f-475b-bd57-f27619723591”
-$MyOrgBIAppID = “84e116a4-bfc9-4940-bbdb-c0d3fc94aa9a”
-$MyOrgBIThumbprint = “E24DF521C12DEDF98752A90A0FA47DD8D3589C9D”
+$PBIAdminUPN = “youremail@email.com.br”
+$PBIAdminPW = “yourpassword”
+$MyOrgTenantID = “your tenant”
+$MyOrgBIAppID = “your ID from your Azure app”
+$MyOrgBIThumbprint = “thumbprint id”
 
-
-#2. Executa a credencial de acordo com o login, senha e tenant ID acima
+# Run the credential according to the login, password and tenant ID above
 $SecPasswd = ConvertTo-SecureString $PBIAdminPW -AsPlainText -Force
 
 $myCred = New-Object System.Management.Automation.PSCredential($PBIAdminUPN,$SecPasswd)
 
-Connect-AzureAD -TenantId “8def9a7d-e13f-475b-bd57-f27619723591” -Credential $myCred
+Connect-AzureAD -TenantId “your Azure tenant” -Credential $myCred
 
-#3. Define o diretório e o nome do arquivo que será exportado o arquivo CSV
+# Defines the directory and name of the file to be exported to the CSV file
 $RetrieveDate = Get-Date 
-$BasePath = "D:\BI_Processos\PowerShell\Azure\"
-$OrgO365LicensesCSV = $BasePath + "I_BI_AZURELICENCAS.csv"
+$BasePath = "D:\Azure\"
+$OrgO365LicensesCSV = $BasePath + "BI_AzureLicenses.csv"
 
 #https://docs.microsoft.com/en-us/office365/enterprise/powershell/view-licenses-and-services-with-office-365-powershell
 $OrgO365Licenses = Get-AzureADSubscribedSku | Select-Object SkuID, SkuPartNumber,CapabilityStatus, ConsumedUnits -ExpandProperty PrepaidUnits | `
